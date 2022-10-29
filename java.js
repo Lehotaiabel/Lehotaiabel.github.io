18864c13cb459d084ea83effee331dd91a29c456
lehotaiabel <116622493+Lehotaiabel@users.noreply.github.com>
2022-10-29 16:59:33 +0200

    new

---
+++ java.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector('.hamburger');
+const hamburger_icon = hamburger.querySelector('span');
+const mobile_menu = document.querySelector('.mobile-menu')
+
+hamburger.addEventListener('click', () => {
+    hamburger_icon.innerText = hamburger_icon.innerText === 'menu'
+        ? 'close'
+        : 'menu';
+        
+    mobile_menu.classList.toggle('is-open');
+})
+
+function myFunction() {
+    var input = document.getElementById("Search");
+    var filter = input.value.toLowerCase();
+    var nodes = document.getElementsByClassName('target');
+  
+    for (i = 0; i < nodes.length; i++) {
+      if (nodes[i].innerText.toLowerCase().includes(filter)) {
+        nodes[i].style.display = "block";
+      } else {
+        nodes[i].style.display = "none";
+      }
+    }
+  }
